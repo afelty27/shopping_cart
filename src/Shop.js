12d16f6b601caf887c0d6e1084a92d2f886137c9
afelty <afelty15@gmail.com>
2022-12-13 19:02:18 -0600

    Add initial fetch items from api function. FIX - api url needed

--- src/Shop.js
+++ src/Shop.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css'
 
 function Shop() {
+
+    useEffect(() => {
+        fetchItems();
+    }, []);
+
+    const fetchItems = async () => {
+        data = await fetch("ENTER API URL HERE");
+        console.log(data);
+    }
+
   return (
     <div className="App">
       Shop Page
