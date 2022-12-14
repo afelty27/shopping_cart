db146a0ceb5460330fe081a31edb975a24c022b4
afelty <afelty15@gmail.com>
2022-12-13 19:02:44 -0600

    Create fetch items function. FIX - need api url

--- src/Nav.js
+++ src/Nav.js
@@ -4,15 +4,23 @@ import {Link} from 'react-router-dom';
 
 
 function Nav() {
+
+    const navStyle = {
+        color: 'white'
+    }
   return (
     <nav>
         <h3>Logo</h3>
         <ul className='nav-links'>
-            <link>
-                {/* <li>About</li> */}
-            </link>
-            <li>Shop</li>
-            <li>Home</li>
+            <li>
+                <Link style={navStyle} to="/about">About</Link>
+            </li>
+            <li>
+                <Link style={navStyle} to="/shop">Shop</Link>
+            </li>
+            <li>
+                <Link style={navStyle} to="/">Home</Link>
+            </li>
         </ul>
     </nav>
   );
