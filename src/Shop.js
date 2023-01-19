075629165b6a752aadbbc0d272db0bd9990b66e1
afelty <afelty15@gmail.com>
2023-01-18 19:35:54 -0600

    Update to allow to compile correctly

--- src/Shop.js
+++ src/Shop.js
@@ -2,23 +2,29 @@ import React, {useState, useEffect} from 'react';
 import './App.css'
 import {Link} from 'react-router-dom';
 
-function Shop() {
+function Shop(props) {
 
-    useEffect(() => {
-        fetchItems();
-    }, []);
+    useEffect( () => {
+        fetchItems()
+    }, [])
 
     const [items,setItems] = useState([]);
 
     const fetchItems = async () => {
         console.log("enter fetchItems")
+
+        //get 10 items from api and set local state
         let data = await fetch("https://fortnite-api.theapinetwork.com/upcoming/get");
         const itemsJson = await data.json();
         const itemsArray = itemsJson.data.splice(0,10);
         console.log(itemsArray);
         setItems(itemsArray);
+
+        //??
+        props.setShopState(itemsArray)
         console.log("exit fetch items")
     }
+
 
     //generate random integer between 0 and max
     //max: integer value
@@ -35,7 +41,7 @@ function Shop() {
 
                    <div key={getRandomInt(10000)} className="card">
                         <div className="container">
-                            <Link to={`/shop/${item.itemId}`}>{item.item.name}</Link>
+                            <Link to={`/shop/${item.itemId}`} >{item.item.name}</Link>
                             <img id='' src={item.item.images.icon}></img>
                             <p>{item.item.description}</p> 
                         </div>
