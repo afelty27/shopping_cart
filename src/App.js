4febfc54436d1df5522fa48ec01ff264005efc0f
afelty <afelty15@gmail.com>
2023-01-26 16:04:11 -0600

    Update CartItemDisplay so it does not access DOM

--- src/App.js
+++ src/App.js
@@ -1,61 +1,59 @@
-import './App.css';
-import React, {useState, useEffect} from 'react';
-import Nav from './Nav';
-import Home from './Home';
-import Shop from './Shop';
-import About from './About';
-import Cart from './Cart';
-import ItemDetail from './ItemDetail';
-import {BrowserRouter as Router, Switch, Route, Routes} from 'react-router-dom';
+//make sure not accessing DOM anywhere
+//save cart to local storage so doesn't go away when refresh
+//make checkoug page that then saves data into a json
+//style
+
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Nav from "./Nav";
+import Home from "./Home";
+import Shop from "./Shop";
+import About from "./About";
+import Cart from "./Cart";
+import ItemDetail from "./ItemDetail";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
-
   //state and update state functions for shop and cart
-  const [shopItems, setShopItems] = useState(
-    {
-    }
-    
-  )
+  const [shopItems, setShopItems] = useState({});
 
   const updateShop = (itemList) => {
-    setShopItems(prevState => {
+    setShopItems((prevState) => {
       return {
-        items: itemList
-      }
-    }) 
-  }
+        items: itemList,
+      };
+    });
+  };
 
-  const [cart, setCart] = useState(
-    {
-      items: [],
-      cartTotal: 0
-    }
-  );
+  const [cart, setCart] = useState({
+    items: [],
+    cartTotal: 0,
+  });
 
-  console.log("cart items: " + cart.items)
+  console.log("cart items: " + cart.items);
 
   //set cart total from start of program
   useEffect(() => {
-    console.log("enter useEffect")
-    console.log(cart)
-     updateCartTotal()
-    
+    console.log("enter useEffect");
+    console.log(cart);
+    updateCartTotal();
   }, [cart["items"]]);
 
- 
-
-  console.log("shop")
-  console.log(shopItems)
-
+  console.log("shop");
+  console.log(shopItems);
 
   //adds new item object to cart item list
   //newProdObj: object from the api call of product being added
   //quantity: integer of how many of the object are requested
   const addProductToCart = (newProdObj, quantity) => {
-  
     //ensure item is not in cart already
     if (findCartItem(newProdObj.itemId) == -1) {
-      console.log("price: " + newProdObj.item.cost)
+      console.log("price: " + newProdObj.item.cost);
       let newObj = {
         itemName: newProdObj.item.name,
         itemId: newProdObj.itemId,
@@ -63,107 +61,93 @@ function App() {
         itemIcon: newProdObj.item.images.icon,
         // itemPrice: (newProdObj.item.cost == "???") ? getRandomInt(50) : newProdObj.item.cost,
         itemPrice: getRandomInt(50),
-        itemQuantity: quantity
-      }
-  
+        itemQuantity: quantity,
+      };
+
       let newList = cart.items;
-      newList.push(newObj)
-  
-      setCart(prevState => {
+      newList.push(newObj);
+
+      setCart((prevState) => {
         return {
           ...prevState,
-          ["items"]: newList
-        }
-      })
+          ["items"]: newList,
+        };
+      });
     } else {
       //what do if already in cart??
     }
 
     updateCartTotal();
-  }
-
-  //try a useEffect to fix the bug below:
-  useEffect( () => {
-
-  }, [])
+  };
 
   //updates the quantity of an object already in cart
   //prodId: string containing id of the desired product
   //newQuantity: integer of new quantity value of the desired product
-  //FIX - takes two clicks of button to update price (state updates after one click)
   const updateProductCart = (prodId, newQuantity) => {
-    console.log("enter updateCart")
-    console.log("prod Id: " + prodId)
-    console.log("new quantity: " + newQuantity)
+    console.log("enter updateCart");
+    console.log("prod Id: " + prodId);
+    console.log("new quantity: " + newQuantity);
 
-    //FIX - setCart is running after updateCartTotal() but its not an asyncronous function?
-    //this is because useState is asyncronous
-    setCart( prevState => {
-      console.log("enter set cart")
+    setCart((prevState) => {
+      console.log("enter set cart");
 
       //ensure item is in cart
       if (findCartItem(prodId) != -1) {
-        console.log("enter if")
+        console.log("enter if");
         const newObj = [...cart.items];
         newObj[findCartItem(prodId)].itemQuantity = newQuantity;
-        console.log("check update quant: ")
-        console.log(newObj[findCartItem(prodId)].itemQuantity)
+        console.log("check update quant: ");
+        console.log(newObj[findCartItem(prodId)].itemQuantity);
 
         return {
           ...prevState,
-          ["items"]: newObj
-        }
+          ["items"]: newObj,
+        };
       }
-    })
+    });
     updateCartTotal();
-  }
-
+  };
 
   //delete particular product from cart state
   //prodId: string containing id of the product to be deleted
   const deleteFromProductCart = (prodId) => {
-
-    setCart(prevState => {
-
+    setCart((prevState) => {
       const newList = [...cart["items"]];
-      newList.splice(findCartItem(prodId),1)
+      newList.splice(findCartItem(prodId), 1);
 
       return {
         ...prevState,
-        ["items"]: newList
-      }
-
-    })
-  }
+        ["items"]: newList,
+      };
+    });
+  };
 
   //updates cart value of state based on current products in cart
   //works
   const updateCartTotal = () => {
-    console.log("enter updateTotal")
+    console.log("enter updateTotal");
     let newTotal = 0;
 
-    for (let i = 0; i < cart.items.length; i ++) {
-      console.log("new quant: " + cart.items[i].itemQuantity)
+    for (let i = 0; i < cart.items.length; i++) {
+      console.log("new quant: " + cart.items[i].itemQuantity);
 
-      newTotal += cart.items[i].itemPrice * cart.items[i].itemQuantity
+      newTotal += cart.items[i].itemPrice * cart.items[i].itemQuantity;
     }
 
-    setCart (prevState => {
+    setCart((prevState) => {
       return {
         ...prevState,
-        ["cartTotal"]: newTotal
-      }
-    })
+        ["cartTotal"]: newTotal,
+      };
+    });
 
-    console.log("newTotal: " + newTotal)
-    
-  }
+    console.log("newTotal: " + newTotal);
+  };
 
   //takes id and searches items list in state for the index of the object with that id. Returns index of correct object, or -1 if not found
   //matchId: string containing id of desired product
   //works
   const findCartItem = (matchId) => {
-
     for (let i = 0; i < cart.items.length; i++) {
       if (cart.items[i].itemId == matchId) {
         return i;
@@ -171,38 +155,72 @@ function App() {
     }
 
     return -1;
-  }
+  };
 
   //generate random integer between 0 and max
   //will be used for products whose value is '???'
   //max: integer value
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }  
-
-
+  }
 
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <Nav />
         <Routes>
-          <Route path='/' exact element={<Home itemState={shopItems} state={cart} addFromProductCart={addProductToCart} deleteFromProductCart={deleteFromProductCart} updateProductCart={updateProductCart}/>} />
-          <Route path='/about' element={<About />} />
-          <Route path='/shop' exact element={<Shop setShopState={updateShop} shopState={shopItems} addProductToCart={addProductToCart} />} />
-          <Route path='/shop/:id' element={<ItemDetail addProductToCart={addProductToCart} />} />
-          <Route path='/cart' element={<Cart userCart={cart} deleteItem={deleteFromProductCart} updateCart={updateProductCart} />} />
+          <Route
+            path="/"
+            exact
+            element={
+              <Home
+                itemState={shopItems}
+                state={cart}
+                addFromProductCart={addProductToCart}
+                deleteFromProductCart={deleteFromProductCart}
+                updateProductCart={updateProductCart}
+              />
+            }
+          />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/shop"
+            exact
+            element={
+              <Shop
+                setShopState={updateShop}
+                shopState={shopItems}
+                addProductToCart={addProductToCart}
+              />
+            }
+          />
+          <Route
+            path="/shop/:id"
+            element={<ItemDetail addProductToCart={addProductToCart} />}
+          />
+          <Route
+            path="/cart"
+            element={
+              <Cart
+                userCart={cart}
+                deleteItem={deleteFromProductCart}
+                updateCart={updateProductCart}
+              />
+            }
+          />
         </Routes>
       </div>
-     
     </Router>
-    
   );
 }
 
 export default App;
-{/* <div className="App">
+{
+  /* <div className="App">
 <Nav />
 <Route path='/home' component={Home}/>
 <Route path='/shop' component={Shop}/>
-</div> */}
+</div> */
+}
+
+//CartItemDisplay -> don't touch DOM, make state and do const
--- src/CartItemDisplay.js
+++ src/CartItemDisplay.js
@@ -1,23 +1,48 @@
-import React from 'react';
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 //FIX - value not updated when click up or down
 
-function cartItemDisplay(props) {
+function CartItemDisplay(props) {
+  const [quantity, setQuantity] = useState(0);
 
   return (
     <div className="cartDisplay">
-        <img id='' src={props.index.itemIcon}></img>
-        <div className='price'>Item Price: ${props.index.itemPrice}</div>
-        <div>Quantity: {props.index.itemQuantity}</div>
-        
-        <input type="number" id="quantity" name="quantity" min="0" max="100" defaultValue={props.index.itemQuantity}></input>
-        <button id='cartUpdateBtn' className='updateBtn' onClick={ () => {props.updateCart(props.index.itemId, document.getElementById("quantity").value)}}>Update Cart</button>
-        <button className='deleteBtn' onClick={ () => {props.deleteItem(props.index.itemId)}}>Delete from Cart</button>
+      <img alt="Product pictue" id="" src={props.index.itemIcon}></img>
+      <div className="price">Item Price: ${props.index.itemPrice}</div>
+      <div>Quantity: {props.index.itemQuantity}</div>
+
+      <input
+        type="number"
+        id="quantity"
+        name="quantity"
+        min="0"
+        max="100"
+        value={quantity}
+        defaultValue={quantity}
+        onChange={(event) => {
+          setQuantity(event.target.value);
+        }}
+      ></input>
+      <button
+        id="cartUpdateBtn"
+        className="updateBtn"
+        onClick={() => {
+          props.updateCart(props.index.itemId, quantity);
+        }}
+      >
+        Update Cart
+      </button>
+      <button
+        className="deleteBtn"
+        onClick={() => {
+          props.deleteItem(props.index.itemId);
+        }}
+      >
+        Delete from Cart
+      </button>
     </div>
   );
-
-
 }
 
-export default cartItemDisplay;
+export default CartItemDisplay;
